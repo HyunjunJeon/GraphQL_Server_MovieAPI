4184913a1e99b18379fc512e6ba4e928a447364d
JeonHyunJun <jeonhj920@ex-em.com>
2019-01-22 21:08:10 +0900

    mutation incompletion

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,40 @@
-export const people = [
-    {
-        id: 0,
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
-      },
-      {
-        id: 1,
-        name: "Jisu",
-        age: 18,
-        gender: "female"
-      },
-      {
-        id: 2,
-        name: "Japan Guy",
-        age: 18,
-        gender: "male"
-      },
-      {
-        id: 3,
-        name: "Daal",
-        age: 18,
-        gender: "male"
-      },
-      {
-        id: 4,
-        name: "JD",
-        age: 18,
-        gender: "male"
-      },
-      {
-        id: 5,
-        name: "moondaddi",
-        age: 18,
-        gender: "male"
-      },
-      {
-        id: 6,
-        name: "flynn",
-        age: 18,
-        gender: "male"
-      }
+let movies = [
+  {
+    id: 0,
+    name: "1111111111111111",
+    score: 1
+  },
+  {
+    id: 1,
+    name: "2222222222222222",
+    score: 2
+  },
+  {
+    id: 2,
+    name: "33333333333333333",
+    score: 3
+  },
+  {
+    id: 3,
+    name: "444444444444444444",
+    score: 4
+  },
+  {
+    id: 4,
+    name: "5555555555555555555",
+    score: 5
+  }
 ]
 
+export const getMovies = () => movies
+
 export const getById = id => {
-    const filterdPeople = people.filter(person => id === person.id)
-    return filterdPeople[0];
+    const filterdMovies = movies.filter(movie => id === movie.id)
+    return filterdMovies[0];
+}
+
+export const deleteMovie = id => {
+    const numOfMovie = movies.findIndex(id)
+    movies = [...movies.slice(0,numOfMovie), ...movies.slice(numOfMovie+1)]
+    return true
 }
