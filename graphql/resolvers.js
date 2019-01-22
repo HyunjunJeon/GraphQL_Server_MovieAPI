e6d39c73acb55a78944f7f3db88b02bd5979bbfa
JeonHyunJun <jeonhj920@ex-em.com>
2019-01-22 20:39:47 +0900

    schema, query, resolver end

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,9 @@
-const hyunjun = {
-    name: "Jeonhyunjun",
-    age: 28,
-    gender: "male"
-}
+import { people, getById } from "./db"
 
 const resolvers = {
     Query: {
-        person: () => hyunjun
+        people: () => people,
+        person: (_, { id }) => getById(id)
     }
 }
 
